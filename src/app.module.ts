0e55c625a053de36fa45426719841ff65e027232
Micael Lima <micael1998lima@gmail.com>
2023-02-04 10:00:08 -0300

    fix: remove unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { typeOrmConfig } from './infra/db/typeorm/config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
