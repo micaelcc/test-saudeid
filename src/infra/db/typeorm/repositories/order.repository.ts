f02ef3dc44c470db309d12091a382bdb5ed1d863
Micael Lima <micael1998lima@gmail.com>
2023-02-04 10:01:48 -0300

    feat: add order repository implementation

---
+++ src/infra/db/typeorm/repositories/order.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { Order } from '../../../../domain/orders/order.entity';
+import { OrdersRepository } from 'src/domain/orders/order.repository';
+import { CreateOrderDTO } from 'src/shared/dtos/create-order.dto';
+import { EntityManager } from 'typeorm';
+
+@Injectable()
+class TypeOrmOrderRepository implements OrdersRepository {
+  constructor(@InjectEntityManager() private readonly manager: EntityManager) {}
+
+  async update(order: Order): Promise<void> {
+    await this.manager.update(Order, order.id, order);
+  }
+
+  async getAll(): Promise<Order[]> {
+    return await this.manager.find(Order);
+  }
+
+  async create(order: CreateOrderDTO): Promise<void> {
+    await this.manager.save(Order, order);
+  }
+}
+
+export { TypeOrmOrderRepository };
