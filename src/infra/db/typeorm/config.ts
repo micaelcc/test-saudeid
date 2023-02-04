d9b40ec05585f0ab6b758de6d996a108c6851439
Micael Lima <micael1998lima@gmail.com>
2023-02-04 10:03:55 -0300

    feat: add new entities in typeorm config

--- src/infra/db/typeorm/config.ts
+++ src/infra/db/typeorm/config.ts
@@ -1,7 +1,10 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { connectionOptions } from 'ormconfig';
+import { Order } from 'src/domain/orders/order.entity';
+import { Product } from 'src/domain/products/product.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
+  entities: [Order, Product],
   ...connectionOptions,
 };
