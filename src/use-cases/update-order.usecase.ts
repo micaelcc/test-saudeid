a96311d25d659683165231b60b9619a3e9127c4d
Micael Lima <micael1998lima@gmail.com>
2023-02-04 15:57:52 -0300

    feat: add update order use case

---
+++ src/use-cases/update-order.usecase.ts
@@ -0,0 +1,32 @@
+import { Inject } from '@nestjs/common';
+import { UpdateOrderDTO } from 'src/shared/dtos/update-order.dto';
+
+import { OrdersRepository } from '../domain/orders/order.repository';
+
+class UpdateOrderUseCase {
+  constructor(
+    @Inject(OrdersRepository)
+    private readonly ordersRepository: OrdersRepository,
+  ) {}
+
+  async execute({ id, data }: UpdateOrderDTO): Promise<void> {
+    const order = await this.ordersRepository.findById(id);
+
+    if (!order) {
+      return;
+    }
+
+    let totalValue = 0;
+
+    data.products?.forEach((product) => {
+      totalValue += product.unitPrice;
+    });
+
+    order.products = data.products;
+    order.totalValue = totalValue;
+
+    await this.ordersRepository.update(order);
+  }
+}
+
+export { UpdateOrderUseCase };
