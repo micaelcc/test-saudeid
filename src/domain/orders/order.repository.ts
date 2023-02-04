6ed331b2ee1f52d1363a97d8462e753d11503099
Micael Lima <micael1998lima@gmail.com>
2023-02-04 10:01:19 -0300

    fix: update order repository must receive an entity

--- src/domain/orders/order.repository.ts
+++ src/domain/orders/order.repository.ts
@@ -1,10 +1,9 @@
 import { CreateOrderDTO } from 'src/shared/dtos/create-order.dto';
-import { UpdateOrderDTO } from 'src/shared/dtos/update-order.dto';
 import { Order } from './order.entity';
 
 export interface OrdersRepository {
   create: (order: CreateOrderDTO) => Promise<void>;
-  update: (updateOrder: UpdateOrderDTO) => Promise<void>;
+  update: (updateOrder: Order) => Promise<void>;
   getAll: () => Promise<Order[]>;
 }
 
