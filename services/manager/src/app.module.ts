7428616b89e02e7417dbce9d602b1c473a25f1bb
Micael Lima <micael1998lima@gmail.com>
2023-02-05 20:22:29 -0300

    feat(manager): add redis client

--- services/manager/src/app.module.ts
+++ services/manager/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ManagerController } from './controllers/manager.controller';
 import { CreateProductService } from './services/create-product.service';
 import { UpdateStockService } from './services/update-stock.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'STOCK_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          host: 'redis',
+          port: 6379,
+        },
+      },
+    ]),
+  ],
   controllers: [ManagerController],
   providers: [CreateProductService, UpdateStockService],
 })
