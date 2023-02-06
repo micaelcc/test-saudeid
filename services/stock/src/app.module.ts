ad0ca00439b27cff7dae0965b3674cd858cf41a2
Micael Lima <micael1998lima@gmail.com>
2023-02-06 12:40:50 -0300

    refactor(stock): move mongo url to env var

--- services/stock/src/app.module.ts
+++ services/stock/src/app.module.ts
@@ -5,8 +5,9 @@ import { CreateProductUseCase } from '@/use-cases/create-product.usecase';
 import { UpdateStockUseCase } from '@/use-cases/update-stock.usecase';
 import { ProductsRepository } from '@/domain/product.repository';
 import { MongoProductsRepository } from '@/infra/db/mongodb/repositories/product.repository';
+import { mongodbConfig } from './infra/db/mongodb/config';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo-container:27017/nest')],
+  imports: [MongooseModule.forRoot(mongodbConfig.MONGO_URL_DEV)],
   controllers: [StockController],
   providers: [
     CreateProductUseCase,
