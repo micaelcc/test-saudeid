c24d7bc768deca45d450898c23e948ad29116882
Micael Lima <micael1998lima@gmail.com>
2023-02-06 19:18:30 -0300

    fix(api-gateway): uuid validator error

--- services/api-gateway/src/presentation/validators/uuid-array-validator.ts
+++ services/api-gateway/src/presentation/validators/uuid-array-validator.ts
@@ -7,7 +7,7 @@ import isUUID from 'validator/lib/isUUID';
 @ValidatorConstraint({ name: 'validateArray', async: false })
 export class ValidateUUIDArray implements ValidatorConstraintInterface {
   validate(data: string[]) {
-    const isNotUUID = data.some((id) => !isUUID(id));
+    const isNotUUID = data?.some((id) => !isUUID(id));
 
     return !isNotUUID;
   }
