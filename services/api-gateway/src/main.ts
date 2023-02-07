2c3a6edf2f4537de255e3bb4e91f854ba6844579
Micael Lima <micael1998lima@gmail.com>
2023-02-06 22:06:22 -0300

    feat(api-gateway): add swagger config

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -1,11 +1,23 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Test SaudeId')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('test-saudeid')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
